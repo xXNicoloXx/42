386fd79df977530ea7b058b25337a8ddad7f9b61
Nicolas GRIVEAU <ngriveau@e3r10p20.clusters.42paris.fr>
2022-11-21 16:25:16 +0100

    finish printf

--- 2.Printf/new/ft_printf.c
+++ 2.Printf/new/ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 20:37:48 by nicolasgriv       #+#    #+#             */
-/*   Updated: 2022/11/21 15:14:17 by ngriveau         ###   ########.fr       */
+/*   Updated: 2022/11/21 16:16:33 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,22 +36,28 @@ int	ft_writestr(char *str, int nbrch)
 	return (nbrch);
 }
 
-int	ft_verif_pc(char str)
+int	ft_verif_pc(const char *str)
 {
-	int	i;
-
-	i = 0;
-	if (str == '%')
-	{
-		if ((str + 1) == 'c' || (str + 1) == 's' || (str + 1) == 'p'
-			|| (str + 1) == 'd' || (str + 1) == 'i' || (str + 1) == 'u'
-			|| (str + 1) == 'x' || (str + 1) == 'X')
-			return (1);
-		else
-			return (0);
-	}
-	else 
+	if (str[1] == 'c')
 		return (1);
+	else if (str[1] == 's')
+		return (1);
+	else if (str[1] == 'p')
+		return (1);
+	else if (str[1] == 'd')
+		return (1);
+	else if (str[1] == 'i')
+		return (1);
+	else if (str[1] == 'u')
+		return (1);
+	else if (str[1] == 'x')
+		return (1);
+	else if (str[1] == 'X')
+		return (1);
+	else if (str[1] == '%')
+		return (1);
+	else
+		return (2);
 }
 
 int	ft_print_pourcent(const char *str, int nbrch, va_list *list)
@@ -74,7 +80,6 @@ int	ft_print_pourcent(const char *str, int nbrch, va_list *list)
 		nbrch = ft_pc_x(va_arg(*list, long), "0123456789ABCDEF", nbrch);
 	if (str[1] == '%')
 		nbrch = ft_writechar('%', nbrch);
-
 	return (nbrch);
 }
 
@@ -87,12 +92,14 @@ int	ft_printf(const char *str, ...)
 	i = -1;
 	nbrch = 0;
 	va_start(list, str);
+	if (str == NULL)
+		return (-1);
 	while (str[++i] != '\0')
 	{
 		if (str[i] == '%' && str[i + 1] == '\0')
 			return (-1);
-		else if (ft_verif_pc(str[i]) == 1)
-			nbrch = ft_writechar(str[i], nbrch);
+		if (str[i] == '%' && ft_verif_pc(&str[i]) == 2)
+			nbrch = ft_writechar('%', nbrch);
 		else if (str[i] == '%')
 			nbrch = ft_print_pourcent(&str[i++], nbrch, &list);
 		else
@@ -100,83 +107,4 @@ int	ft_printf(const char *str, ...)
 	}
 	va_end(list);
 	return (nbrch);
-}
-
-
-
-
-
-int	main(void)
-{
-	int		original;
-	int		mine;
-	char	*str;
-
-	str = "Just some text..";
-
-	printf("\n\n--- ALL ---\n");
-	original = printf("-> %c | %s | %p | %d | %i | %u | %x | %X | %% |", str[0], str, str, 42, 42, 42, 42, 42);
-	printf("\n");
-	mine = ft_printf("-> %c | %s | %p | %d | %i | %u | %x | %X | %% |", str[0], str, str, 42, 42, 42, 42, 42);
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- SAME WITH %%%%%% AT THE END ---\n");
-	original = printf("-> %c | %s | %p | %d | %i | %u | %x | %X | %% |%%%", str[0], str, str, 42, 42, 42, 42, 42);
-	printf("\n");
-	mine = ft_printf("-> %c | %s | %p | %d | %i | %u | %x | %X | %% |%%%", str[0], str, str, 42, 42, 42, 42, 42);
-	printf("\n");
-	printf("%d  | %d\n", original, mine);
-
-	printf("\n\n--- SAME WITH %%%%%% AT THE END AND SOME TEXT ---\n");
-	original = printf("-> %c | %s | %p | %d | %i | %u | %x | %X | %% |%%%| texte", str[0], str, str, 42, 42, 42, 42, 42);
-	printf("\n");
-	mine = ft_printf("-> %c | %s | %p | %d | %i | %u | %x | %X | %% |%%%| texte", str[0], str, str, 42, 42, 42, 42, 42);
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- EMPTY STRING ---\n");
-	original = printf("");
-	mine = ft_printf("");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- WITH ft_printf(0) ---\n");
-	original = printf(0);
-	mine = ft_printf(0);
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- WITH STR AS ONLY ARGUMENT ---\n");
-	original = printf(str);
-	printf("\n");
-	mine = ft_printf(str);
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- WITH NULL AS STRING AND ADRESS ---\n");
-	original = printf("-> %s | %p", NULL, NULL);
-	printf("\n");
-	mine = ft_printf("-> %s | %p", NULL, NULL);
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- WITH TAB AND ANTICIPATED END OF STRING ---\n");
-	original = printf("-> \t|\0\0gloubiboulgah");
-	printf("\n");
-	mine = ft_printf("-> \t|\0\0gloubiboulgah");
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- WITH NEGATIVE VALUES ---\n");
-	original = printf("%u | %x | %X | %d | %i | %c | %p", -42, -42, -42, -42, -42, -42, (void *) -42);
-	printf("\n");
-	mine = ft_printf("%u | %x | %X | %d | %i | %c | %p", -42, -42, -42, -42, -42, -42, (void *) -42);
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
-
-	printf("\n\n--- PRINT ADRESS WITH %%x and %%X ---\n");
-	original = printf("%x | %X | %x | %X", NULL, NULL, &str, &str);
-	printf("\n");
-	mine = ft_printf("%x | %X | %x | %X", NULL, NULL, &str, &str);
-	printf("\n");
-	printf("%d  : %d\n", original, mine);
 }
--- 2.Printf/new/ft_printf.h
+++ 2.Printf/new/ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/17 20:40:30 by nicolasgriv       #+#    #+#             */
-/*   Updated: 2022/11/21 14:26:16 by ngriveau         ###   ########.fr       */
+/*   Updated: 2022/11/21 16:15:57 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,21 +39,17 @@
 
 int		ft_writechar(char ch, int nbrch);
 int		ft_writestr(char *str, int nbrch);
-int     ft_print_pourcent(const char *str, int nbrch, va_list *list);
+int		ft_print_pourcent(const char *str, int nbrch, va_list *list);
 int		ft_printf(const char *str, ...);
-
 int		ft_pc_id(int nbr, int nbrch);
 void	ft_putnbr_pc_id(int nb);
 void	ft_putchar_pc_idu(char c);
-
 int		ft_pc_p(unsigned long nbr, int nbrch);
 int		ft_print_pc_p_base(unsigned long nbr, int nbrch);
-int     ft_len_nbr_pc_p(unsigned long nbr, int nbrch);
-
+int		ft_len_nbr_pc_p(unsigned long nbr, int nbrch);
 int		ft_pc_u(unsigned int nbr, int nbrch);
 void	ft_putnbr_pc_u(unsigned int nb);
-
-int     ft_pc_x(long nbr, char *base, int nbrch);
-int     ft_len_nbr_pc_x(unsigned int nbr, int nbrch);
+int		ft_pc_x(long nbr, char *base, int nbrch);
+int		ft_len_nbr_pc_x(unsigned int nbr, int nbrch);
 void	ft_print_base_pc_x(unsigned int nbr, char *base);
 #endif
