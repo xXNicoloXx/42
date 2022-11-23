4c331dbefe8786de6ee41dbfb243cbc64662e98d
Nicolas GRIVEAU <ngriveau@e3r10p21.clusters.42paris.fr>
2022-11-23 19:10:40 +0100

    23.11.22-19:10:40

---
+++ 99 - Finish/1. Libft 108%/ft_substr.c
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/09 11:54:39 by ngriveau          #+#    #+#             */
+/*   Updated: 2022/11/15 19:53:03 by ngriveau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	int		i;
+	char	*str;
+
+	i = 0;
+	if (len < start || ((size_t) ft_strlen(s)) <= start)
+		return (ft_strdup(""));
+	if (((size_t) ft_strlen(s)) <= len)
+		str = malloc(sizeof(char) * (ft_strlen(s) - start + 1));
+	else
+		str = malloc(sizeof(char) * (len + 1));
+	if (str == NULL)
+		return (NULL);
+	while (len > 0 && s[start] != '\0')
+	{
+		str[i] = s[start];
+		i++;
+		start++;
+		len--;
+	}
+	str[i] = '\0';
+	return (str);
+}
+
+// int main (void)
+// {
+// 	char *test = "123456789";
+// 	int start = 8;
+// 	int len = 10;
+// 	printf("ft = %s\n",ft_substr(test,start,len));
+// }
