57eb66414d6709159a3c987f9d66d3e99d44e627
Nicolas GRIVEAU <ngriveau@e1r13p14.clusters.42paris.fr>
2022-12-06 16:51:07 +0100

    06.12.22-16:51:07

--- 5.Fdf/test.c
+++ 5.Fdf/test.c
@@ -6,7 +6,7 @@
 /*   By: ngriveau <ngriveau@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 18:36:35 by ngriveau          #+#    #+#             */
-/*   Updated: 2022/12/06 16:40:38 by ngriveau         ###   ########.fr       */
+/*   Updated: 2022/12/06 16:51:06 by ngriveau         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -160,5 +160,5 @@ int main (void)
 		y++;
 		x = 0;
 	}
-	mlx_loop(m.mlx);
+	mlx_loop(m.mlx);ssssssss
 }
