e2fbcb1955dc8f187e21a7d9bd639244d82bbd48
Nico <oui@oui.com>
2022-12-22 20:29:18 +0100

    avant norminette

--- 5.Fdf/color.c
+++ 5.Fdf/color.c
@@ -13,52 +13,52 @@
 #include "fdf.h"
 #include "mlx.h"
 
-void ft_color(int nbr, t_map *m)
+void	ft_color(int nbr, t_map *m)
 {
-    int tmp;
+	int tmp;
 
-    tmp = nbr/1048576;
-    nbr = nbr - 1048576 * tmp;
-    m->img.r = tmp * 16;
-    tmp = nbr/65536;
-    nbr = nbr - 65536 * tmp;
-    m->img.r += tmp;
-    tmp = nbr/4096;
-    nbr = nbr - 4096 * tmp;
-    m->img.g = tmp * 16;
-    tmp = nbr/256;
-    nbr = nbr - 256 * tmp;
-    m->img.g += tmp;
-    tmp = nbr/16;
-    nbr = nbr - 16 * tmp;
-    m->img.b = tmp * 16;
-    m->img.b += nbr;
+	tmp = nbr/1048576;
+	nbr = nbr - 1048576 * tmp;
+	m->img.r = tmp * 16;
+	tmp = nbr/65536;
+	nbr = nbr - 65536 * tmp;
+	m->img.r += tmp;
+	tmp = nbr/4096;
+	nbr = nbr - 4096 * tmp;
+	m->img.g = tmp * 16;
+	tmp = nbr/256;
+	nbr = nbr - 256 * tmp;
+	m->img.g += tmp;
+	tmp = nbr/16;
+	nbr = nbr - 16 * tmp;
+	m->img.b = tmp * 16;
+	m->img.b += nbr;
 }
 
 void ft_draw(t_map *m, float x, float y, int color)
 {
-    unsigned long pixel;
+	unsigned long pixel;
 	x = roundf(x);
 	y = roundf(y);
-    ft_color(color, m);
-    pixel = (y * m->img.size) + (x * 4);
+	ft_color(color, m);
+	pixel = (y * m->img.size) + (x * 4);
 	if ((m->img.size) * m->winy < pixel - 5 || x <= 0 || y <= 0 || y > m->y * m->winy || x > m->winx)
 		return ;
 	
-    if (m->img.e == 1)
-    {
-        m->img.data[pixel + 0] = 0;
-        m->img.data[pixel + 1] = m->img.r;
-        m->img.data[pixel + 2] = m->img.g;
-        m->img.data[pixel + 3] = m->img.b;
-    }
-    else if (m->img.e == 0)
-    {
-        m->img.data[pixel + 0] = m->img.b;
-        m->img.data[pixel + 1] = m->img.g;
-        m->img.data[pixel + 2] = m->img.r;
-        m->img.data[pixel + 3] = 0;
-    }
+	if (m->img.e == 1)
+	{
+		m->img.data[pixel + 0] = 0;
+		m->img.data[pixel + 1] = m->img.r;
+		m->img.data[pixel + 2] = m->img.g;
+		m->img.data[pixel + 3] = m->img.b;
+	}
+	else if (m->img.e == 0)
+	{
+		m->img.data[pixel + 0] = m->img.b;
+		m->img.data[pixel + 1] = m->img.g;
+		m->img.data[pixel + 2] = m->img.r;
+		m->img.data[pixel + 3] = 0;
+	}
 }
 
 void ft_tab_deg(t_map *m, int start, int end)
@@ -103,23 +103,44 @@ void ft_tab_color(t_map *m)
 	}
 }
 
-
-void ft_init_color(t_map *m)
+void ft_setup_color_pt2(t_map *m)
 {
-	int i;
-
-	i = 0;
-	while(i < 100)
+	if (m->setupcolor == 4)
 	{
-		m->color[i] = 0;
-		i++;
+		m->color[0] = 0x333333;
+		m->color[99] = 0xffffff;
 	}
-	if (m->setupcolor > 5)
-		m->setupcolor = 0;
+	else if (m->setupcolor == 5)
+	{
+		m->color[0] = 0xff0000;
+		m->color[15] = 0xffa500;
+		m->color[30] = 0xffff00;
+		m->color[45] = 0x008000;
+		m->color[60] = 0x0000ff;
+		m->color[75] = 0x4b0082;
+		m->color[99] = 0xee82ee;
+	}
+	else if (m->setupcolor == 6)
+	{
+			m->color[0] = 0x000001;
+	m->color[10] = 0x004C75;
+	// m->color[61] = 0x0094E3;
+	m->color[59] = 0x0094E3;
+	m->color[61] = 0xe0cda9;
+	m->color[63] = 0x72CC00;
+	m->color[70] = 0x298000;			//worldmap
+	m->color[78] = 0x4f3000;
+	m->color[80] = 0xffffff;
+	m->color[99] = 0xffffff;
+	}
+}
+
+void ft_setup_color_pt1(t_map *m)
+{
 	if (m->setupcolor == 0)
 	{
-		m->color[0] = 0x000001;
-		m->color[99] = 0xffffff;
+		m->color[0] = 0x0000ff;
+		m->color[99] = 0xff0000;
 	}
 	else if (m->setupcolor == 1)
 	{
@@ -136,34 +157,33 @@ void ft_init_color(t_map *m)
 		m->color[0] = 0x00ff00;
 		m->color[99] = 0xff00ff;
 	}
-	else if (m->setupcolor == 4)
+	else
+		ft_setup_color_pt2(m);
+}
+
+void ft_init_color(t_map *m)
+{
+	int i;
+
+	i = 0;
+	while(i < 100)
 	{
-		m->color[0] = 0x333333;
-		m->color[99] = 0xffffff;
+		m->color[i] = 0;
+		i++;
 	}
-	else if (m->setupcolor == 5)
-	{
-		m->color[0] = 0xff0000;
-		m->color[15] = 0xffa500;
-		m->color[30] = 0xffff00;
-		m->color[45] = 0x008000;
-		m->color[60] = 0x0000ff;
-		m->color[75] = 0x4b0082;
-		m->color[99] = 0xee82ee;
-	}
+	if (m->setupcolor > 6)
+		m->setupcolor = 0;
+	ft_setup_color_pt1(m);
 	ft_tab_color(m);
 }
 // int main(void)
 // {
-//     t_map m;
-//     ft_color(0xffffff, &m);
+//	 t_map m;
+//	 ft_color(0xffffff, &m);
 //     printf("r = %d\n",m.img.r);
 //     printf("g = %d\n",m.img.g);
 //     printf("b = %d\n",m.img.b);
 // }
-
-
-
 	// m->color[0] = 0x000001;
 	// m->color[10] = 0x004C75;
 	// m->color[61] = 0x0094E3;
@@ -174,6 +194,5 @@ void ft_init_color(t_map *m)
 	// m->color[78] = 0x4f3000;
 	// m->color[80] = 0xffffff;
 	// m->color[99] = 0xffffff;
-
 	// m->color[0] = 0xff0000;		//42
 	// m->color[99] = 0xffffff;
