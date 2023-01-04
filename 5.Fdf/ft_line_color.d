563a1a933599ce9afb463a4d061ada9a032c56b4
Nicolas GRIVEAU <ngriveau@e1r11p22.clusters.42paris.fr>
2023-01-04 23:51:21 +0100

    .d

---
+++ 5.Fdf/ft_line_color.d
@@ -0,0 +1,2 @@
+ft_line_color.o: ft_line_color.c fdf.h libft.h get_next_line.h \
+ lib/minilibx-linux/mlx.h
---
+++ 5.Fdf/ft_map.d
@@ -0,0 +1,1 @@
+ft_map.o: ft_map.c fdf.h libft.h get_next_line.h lib/minilibx-linux/mlx.h
