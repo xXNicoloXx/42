a11fdaa60255f00bfb149b74536d5b0a0fc80139
Nicolas GRIVEAU <ngriveau@e1r11p22.clusters.42paris.fr>
2023-01-05 01:06:35 +0100

    05/01.01:06

--- 5.Fdf/ft_animation.d
+++
@@ -1,2 +0,0 @@
-ft_animation.o: ft_animation.c fdf.h libft.h get_next_line.h \
- lib/minilibx-linux/mlx.h
--- 5.Fdf/ft_start.d
+++
@@ -1,2 +0,0 @@
-ft_start.o: ft_start.c fdf.h libft.h get_next_line.h \
- lib/minilibx-linux/mlx.h
